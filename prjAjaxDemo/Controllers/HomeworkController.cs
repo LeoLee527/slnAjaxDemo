b57a1874bed624cba81b48e45e8d8969c2543fd7
Leo <lj980804@gmail.com>
2022-04-21 13:52:28 +0800

    加入專案檔案。

---
+++ prjAjaxDemo/Controllers/HomeworkController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjAjaxDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjAjaxDemo.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        private readonly DemoContext _constr;
+
+        public HomeworkController(DemoContext constr)
+        {
+            _constr = constr;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Member(string name)
+        {
+            _constr.Members.Where(m => m.Name == name).FirstOrDefault();
+            return View();
+        }
+    }
+}
